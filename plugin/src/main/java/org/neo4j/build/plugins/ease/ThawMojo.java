74f8e3a007c6bf64907d631088ce4d751fb04162
Anders Nawroth <anders@neotechnology.com>
2012-05-03 11:19:03 +0200

    Added option for including or excluding transitive dependencies.

--- plugin/src/main/java/org/neo4j/build/plugins/ease/ThawMojo.java
+++ plugin/src/main/java/org/neo4j/build/plugins/ease/ThawMojo.java
@@ -33,6 +33,7 @@ import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.MavenArtifactRepository;
 import org.apache.maven.artifact.resolver.ArtifactCollector;
 import org.apache.maven.artifact.resolver.filter.AndArtifactFilter;
+import org.apache.maven.artifact.resolver.filter.ArtifactFilter;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -69,6 +70,13 @@ public class ThawMojo extends AbstractMojo
      * @parameter
      */
     protected List<String> excludes;
+
+    /**
+     * If we should exclude transitive dependencies
+     * 
+     * @parameter expression="${excludeTransitive}" default-value="false"
+     */
+    protected boolean excludeTransitive;
 
     /**
      * Local repo location for fetching dependencies to thaw. When this is
@@ -279,7 +287,6 @@ public class ThawMojo extends AbstractMojo
 
     private Set<Artifact> getDependencies() throws MojoExecutionException
     {
-        HashSet<Artifact> artifacts = new HashSet<Artifact>();
 
         AndArtifactFilter filters = new AndArtifactFilter();
         if ( includes != null )
@@ -291,6 +298,24 @@ public class ThawMojo extends AbstractMojo
             filters.add( new StrictPatternExcludesArtifactFilter( excludes ) );
         }
 
+        Set<Artifact> artifacts = null;
+
+        if ( excludeTransitive )
+        {
+            artifacts = getFilteredDirectDependencies( filters );
+        }
+        else
+        {
+            artifacts = getFilteredTransitiveDependencies( filters );
+        }
+
+        return artifacts;
+    }
+
+    private Set<Artifact> getFilteredTransitiveDependencies(
+            ArtifactFilter filters ) throws MojoExecutionException
+    {
+        HashSet<Artifact> artifacts = new HashSet<Artifact>();
         DependencyNode rootNode = null;
         try
         {
@@ -319,6 +344,20 @@ public class ThawMojo extends AbstractMojo
         }
         // remove this project, which is the root node of the tree
         artifacts.remove( project.getArtifact() );
+        return artifacts;
+    }
+
+    private Set<Artifact> getFilteredDirectDependencies( ArtifactFilter filter )
+    {
+        Set<Artifact> directDependencies = project.getDependencyArtifacts();
+        HashSet<Artifact> artifacts = new HashSet<Artifact>();
+        for ( Artifact artifact : directDependencies )
+        {
+            if ( filter.include( artifact ) )
+            {
+                artifacts.add( artifact );
+            }
+        }
         return artifacts;
     }
 }
